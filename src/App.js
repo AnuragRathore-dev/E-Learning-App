03e47c3cdb1b32a3fc24a67e3a406a12e3a88970
AnuragRathore-dev <coderanucoder@gmail.com>
2024-10-21 21:46:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
-
+ 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Details from "./pages/app/details/Details";
 import Learn from "./pages/app/learn/Learn";
